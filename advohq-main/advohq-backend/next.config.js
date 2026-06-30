3a1235344c189cf866e093a8fd3c46458e21f97c
AdvoHQ <advohq.main@gmail.com>
2026-06-30 15:15:50 +0530

    Delete advohq-main directory

--- advohq-main/advohq-backend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@neondatabase/serverless', 'bcryptjs'],
-  },
-};
-
-module.exports = nextConfig;
